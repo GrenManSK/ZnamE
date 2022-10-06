410b1dba3be2c27238e185f2f0ec054ed147ab43
GrenManSK <melnicek.richard@gmail.com>
2022-10-06 15:50:42 +0200

    Bug fix
    - updating should be working

--- edupage.py
+++ edupage.py
@@ -71,7 +71,7 @@ if nenajdene:
     if args.language == "JP":
         print("番組再開！！！")
     sleep(1)
-    subprocess.check_call('start python edupage.py -lang ' + args.language, shell=True)
+    subprocess.call([sys.executable, os.path.realpath(__file__)] + sys.argv[1:])
     quit()
 os.system('Title ' + 'ZnámE')
 from threading import Thread
@@ -85,7 +85,7 @@ url = 'https://raw.githubusercontent.com/GrenManSK/ZnamE/main/version'
 page = requests.get(url)
 verzia = open('version', 'r')
 
-updateapp = str('import argparse, shutil, os, subprocess, configparser, sys\nfrom time import sleep\nUNSPECIFIED = object()\nglobal parser\nparser = argparse.ArgumentParser()\nparser.add_argument(\'-ef\', \'--endf\', help=\'Will not automatically end program\', default=UNSPECIFIED, nargs=\'?\')\nparser.add_argument(\'-lang\', \'--language\', choices=[\'SK\',\'EN\',\'JP\'], help=\'Language selection\', nargs=\'?\')\nparser.add_argument(\'input\', help=\'Input folder\', nargs=\'?\')\nargs = parser.parse_args()\nconfig = configparser.RawConfigParser()\nconfig.read(\'config.ini\')\nargs.language = config.get(\'basic info\', \'lang\').split(\' \')[0]\nif args.input != "":\n    sleep(0.5)\n    shutil.move(\'edupage.py\', \'old/edupage.py\')\n    shutil.move(args.input + \'/edupage.py\', \'edupage.py\')\n    sleep(0.2)\n    shutil.rmtree(args.input)\n    if args.endf == None:\n        subprocess.check_call(sys.executable + \' edupage.py -lang \' + args.language + \' -endf -update\', shell=True)\n    else:\n        subprocess.check_call(sys.executable + \' edupage.py -lang \' + args.language + \' -update\', shell=True)\n    shutil.rmtree(\'old\')\n    quit()')
+updateapp = str('import argparse, shutil, os, subprocess, configparser, sys\nfrom time import sleep\nUNSPECIFIED = object()\nglobal parser\nparser = argparse.ArgumentParser()\nparser.add_argument(\'-ef\', \'--endf\', help=\'Will not automatically end program\', default=UNSPECIFIED, nargs=\'?\')\nparser.add_argument(\'-lang\', \'--language\', choices=[\'SK\',\'EN\',\'JP\'], help=\'Language selection\', nargs=\'?\')\nparser.add_argument(\'input\', help=\'Input folder\', nargs=\'?\')\nargs = parser.parse_args()\nconfig = configparser.RawConfigParser()\nconfig.read(\'config.ini\')\nargs.language = config.get(\'basic info\', \'lang\').split(\' \')[0]\nif args.input != "":\n    sleep(0.5)\n    shutil.move(\'edupage.py\', \'old/edupage.py\')\n    shutil.move(args.input + \'/edupage.py\', \'edupage.py\')\n    sleep(0.2)\n    shutil.rmtree(args.input)\n    if args.endf == None:\n        subprocess.call(sys.executable + \' edupage.py -lang \' + args.language + \' -endf -update\', shell=True)\n    else:\n        subprocess.call(sys.executable + \' edupage.py -lang \' + args.language + \' -update\', shell=True)\n    shutil.rmtree(\'old\')\n    quit()')
 
 if  semantic_version.Version(page.text[1:]) <= semantic_version.Version(verzia.read()[1:]):
     pass
