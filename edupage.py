d1740d3e4cfb7f4055054a2444676c000f544aa5
GrenManSK <melnicek.richard@gmail.com>
2023-05-17 18:21:00 +0200

    #31 Fixed

--- edupage.py
+++ edupage.py
@@ -1179,6 +1179,11 @@ try:  # type: ignore
                             continue
                         run_textractor(args, translator_vstup, config['basic info']['translator'])
                         translator = True
+                        if waifu or neko or waifuvid:
+                            window = pygetwindow.getWindowsWithTitle('tmp')[0]
+                            window.activate()
+                            window = pygetwindow.getWindowsWithTitle('ZnámE')[0]
+                            window.activate()
                         if logged:
                             completer(unlogged_completer)
                         else:
@@ -1466,6 +1471,9 @@ try:  # type: ignore
                                        end='\r', ttime=0.01)
                             os.remove('assets/waifu.mp4')
                         else:
+                            if translator:
+                                window = pygetwindow.getWindowsWithTitle('tmp')[0]
+                                window.activate()
                             typewriter('Closing image', end='\r', ttime=0.01)
                             pg.keyDown('alt')
                             pg.press('tab')
@@ -1653,6 +1661,11 @@ try:  # type: ignore
                         pg.keyUp('alt')
                     if neko or waifu:
                         if not waifuvid:
+                            if translator:
+                                window = pygetwindow.getWindowsWithTitle('tmp')[0]
+                                window.activate()
+                                window = pygetwindow.getWindowsWithTitle('ZnámE')[0]
+                                window.activate()
                             pg.keyDown('alt')
                             pg.press('tab')
                             pg.keyUp('alt')
